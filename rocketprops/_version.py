7063617dccef1a121a6c2898dc75420394891299
Charlie Taylor <sonofeft@users.noreply.github.com>
2023-01-20 18:32:22 -0800

    Update _version.py

--- rocketprops/_version.py
+++ rocketprops/_version.py
@@ -1,5 +1,5 @@
 
-__version__ = '0.1.5'  # METADATA_RESET:__version__ = '<<version>>'
+__version__ = '0.1.6'  # METADATA_RESET:__version__ = '<<version>>'
 
 """
 The ``_version.py`` file can be executed with execfile to create a local variable __version__::
